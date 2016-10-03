636f8ea1de67608711663883c03927e0ccde7ab4
swarnaa <sbalakri@neiu.edu>
2016-10-03 16:51:07 -0500

    ROR DAY 3

--- Neiugram/app/controllers/posts_controller.rb
+++ Neiugram/app/controllers/posts_controller.rb
@@ -1,6 +1,25 @@
 class PostsController < ApplicationController
 
 def index
+  @posts = Post.all
+end
 
+def new 
+  @post = Post.new
+end
+
+def create
+  @post = Post.create(post_parama)
+  redirect_to posts_path
+end
+
+
+
+
+
+private
+ 
+def post_params
+   params.require(:post).permit(:image, :caption)
 end
 end
